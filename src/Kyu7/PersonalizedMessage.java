2569de76a8a1398ed22587b4215e99afff46b425
Jasminoza <testbot91@gmail.com>
2022-02-13 18:36:13 +0300

    1 task added. Easy.

---
+++ src/Kyu7/PersonalizedMessage.java
@@ -0,0 +1,9 @@
+package Kyu7;
+
+public class PersonalizedMessage {
+
+    static String greet(String name, String owner) {
+        return name.equals(owner)? "Hello boss" : "Hello guest";
+    }
+
+}
