eeedc5948a21380c1b6811ad08f67ebc308927ba
Jasminoza <testbot91@gmail.com>
2022-02-13 18:31:15 +0300

    1 task added. Easy.

---
+++ src/Kyu7/BasicOperations.java
@@ -0,0 +1,22 @@
+package Kyu7;
+
+public class BasicOperations {
+    public static void main(String[] args) {
+        String op = "+";
+        int v1 = 4;
+        int v2 = 7;
+        System.out.println(basicMath(op, v1, v2));
+    }
+
+    public static Integer basicMath(String op, int v1, int v2) {
+        switch (op) {
+            case "+":
+                return v1 + v2;
+            case "-":
+                return v1 - v2;
+            case "*":
+                return v1 * v2;
+        }
+         return v1 / v2;
+    }
+}
