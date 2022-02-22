3cf70929e98826666b716b412d68557c9c2778ca
Jasminoza <testbot91@gmail.com>
2022-02-22 20:59:46 +0300

    1 task added

---
+++ src/Kyu8/MultiplyingTwoNumbers.java
@@ -0,0 +1,14 @@
+package Kyu8;
+
+public class MultiplyingTwoNumbers {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 2;
+        num2 = 2;
+        System.out.println(multiply(num1, num2));
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
